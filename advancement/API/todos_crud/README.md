43dc8d2a1eb56527576a0fd2f2252a5efdea8522
Dominic Bett <dbett49@gmail.com>
2021-07-14 23:43:21 +0300

    Delete advancement/API/todos_crud directory

--- advancement/API/todos_crud/README.md
+++
@@ -1,3 +0,0 @@
-# TODOS API
-
-This is a simple REST/CRUD api that keeps a list of things that a user requires to do in the future.
--- advancement/API/todos_crud/app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const todo = require('./todoController');
-const todoList = require('./todoListController');
-
-module.exports = (app, db) => {
-    todo(app, db);
-    todoList(app, db);
-}
